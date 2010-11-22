0b30fc6b241fcb20f11f498f5de8c61bd349555a
michelH <hognerud@kth.se>
2010-11-22 11:14:05 +0100

    Full Project

---
+++ src/org/nebula/userData/Profile.java
@@ -0,0 +1,148 @@
+package org.nebula.userData;
+
+public class Profile {
+	private int id;
+	private String username;
+	private String domain;
+	private String fullName;
+	private String status;
+	private String phoneNumber;
+	private String sipURI;
+	private String address;
+	private String password;
+	private String email_address;
+	
+	public Profile() {
+		int id = 0;
+		username = "";
+		domain = "";
+		fullName = "";
+		status = "";
+		phoneNumber = "";
+		sipURI = "";
+		address = "";
+		password = "";
+		email_address = "";
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the domain
+	 */
+	public String getDomain() {
+		return domain;
+	}
+	/**
+	 * @param domain the domain to set
+	 */
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+	/**
+	 * @return the fullName
+	 */
+	public String getFullName() {
+		return fullName;
+	}
+	/**
+	 * @param fullName the fullName to set
+	 */
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	/**
+	 * @return the sipURI
+	 */
+	public String getSipURI() {
+		return sipURI;
+	}
+	/**
+	 * @param sipURI the sipURI to set
+	 */
+	public void setSipURI(String sipURI) {
+		this.sipURI = sipURI;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the email_address
+	 */
+	public String getEmail_address() {
+		return email_address;
+	}
+	/**
+	 * @param email_address the email_address to set
+	 */
+	public void setEmail_address(String email_address) {
+		this.email_address = email_address;
+	}
+}

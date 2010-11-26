2a4341339bf4a4c813bdd2a749e77b42ffcf90a7
mavs87 <mavs87@gmail.com>
2010-11-26 10:40:16 +0100

    Refactoring the class

--- src/org/nebula/client/rest/RESTGroupManager.java
+++ src/org/nebula/client/rest/RESTGroupManager.java
@@ -7,6 +7,7 @@ package org.nebula.client.rest;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,6 +15,7 @@ import org.apache.http.client.ClientProtocolException;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.nebula.models.Group;
+import org.nebula.models.Profile;
 
 public class RESTGroupManager extends Resource {
 
@@ -35,5 +37,102 @@ public class RESTGroupManager extends Resource {
 
 		return myGroups;
 	}
+	
+	/**
+	*
+	* @param group group information for the one to add + (contacts to add inside the group)
+	* @return the response from the server (HTTPstatus + message)
+	 * @throws JSONException 
+	 * @throws IOException 
+	 * @throws ClientProtocolException 
+	*/
+	public Status addNewGroup(Group group) throws ClientProtocolException, IOException, JSONException {
+		//group insertion
+		HashMap<String, Object> hM = new HashMap<String, Object>();
+		hM.put("groupName", group.getGroupName());
+		hM.put("status", group.getGroupStatus());
+		
+		Response r = this.post("insertGroup", hM);
+		//201 = HTTP status code for user inserted
+		boolean validOP = r.getStatus() == 201;
+		Status status = new Status(validOP, "" + r.getResult());
+		//store the id of the brand new group for utilize it in inserting the user
+		group.setId(Integer.parseInt(r.getResult().getString("id")));
+		return status;
+	}
+	
+	public Status retrieveGroup(Group g) throws JSONException, ClientProtocolException, IOException {
+		HashMap<String, String> hM = new HashMap<String, String>();
+		hM.put("id", "" + g.getId());
+		Response r = this.get("retrieveGroup", hM);
+		g.setGroupName("" + r.getResult().get("groupName"));
+		g.setGroupStatus("" + r.getResult().get("status"));
+		boolean validOP = (r.getStatus() >= 200) && (r.getStatus() < 300);
+		Status status = new Status(validOP, "" + r.getResult());
+		return status;
+	} 
+	
+	public List<Group> retrieveGroups() throws JSONException, ClientProtocolException, IOException {
+		Response r = this.get("retrieveGroups");
+		List<Group> myGroups = new ArrayList<Group>();
+		
+		for (int i=0; i < r.getResult().length();i++)
+		{
+			Group group = new Group();
+			JSONObject jSonObj = r.getResult().getJSONObject("" + i);
+			group.setGroupName(jSonObj.getString("groupName"));
+			group.setId(jSonObj.getInt("id"));
+			group.setGroupStatus(jSonObj.getString("status"));
+			myGroups.add(group);
+		}
+		return myGroups;
+	} 
+	
+	public Status insertUsersIntoGroup(Group group) throws ClientProtocolException, IOException, JSONException{
+		//for every user, perform the insert operation
+		Response r = null;
+		List<Profile> user = group.getContacts();
+		for (int i=0; i<user.size(); i++)
+		{
+			HashMap<String, Object> hMGroupUser = new HashMap<String, Object>();
+			hMGroupUser.put("userID", user.get(i).getId());
+			hMGroupUser.put("groupID", group.getId());
+			r = this.post("insertUserIntoGroup", hMGroupUser);
+		}
+		boolean validOP = (r.getStatus() == 201);
+		Status status = new Status(validOP, "" + r.getResult());
+		return status;
+	}
+	
+	public Status insertUserIntoGroup(Group group, String username) throws ClientProtocolException, IOException, JSONException {
+		HashMap<String, Object> hMGroupUser = new HashMap<String, Object>();
+		hMGroupUser.put("groupID", group.getId());
+		hMGroupUser.put("username", username);
+		Response r = this.post("insertUserIntoGroup", hMGroupUser);
+		boolean validOP = (r.getStatus() == 201);
+		Status status = new Status(validOP, "" + r.getResult());
+		return status;
+	}
+	
 
+	public Status addContact(String contactUsername) throws ClientProtocolException, IOException, JSONException {
+		HashMap<String, Object> hMGroupUser = new HashMap<String, Object>();
+		hMGroupUser.put("username", contactUsername);
+		hMGroupUser.put("nickname", contactUsername);
+		Response r = this.post("addContact", hMGroupUser);
+		boolean validOP = (r.getStatus() == 201);
+		Status status = new Status(validOP, "" + r.getResult());
+		return status;
+	}
+
+	public Status addContact(String contactUsername, String contactNickName) throws ClientProtocolException, IOException, JSONException {
+		HashMap<String, Object> hMGroupUser = new HashMap<String, Object>();
+		hMGroupUser.put("username", contactUsername);
+		hMGroupUser.put("nickname", contactNickName);
+		Response r = this.post("addContact", hMGroupUser);
+		boolean validOP = (r.getStatus() == 201);
+		Status status = new Status(validOP, "" + r.getResult());
+		return status;
+	}
+	
 }

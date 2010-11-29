f2d90d6f5e19ee9d071f66746e5bf5bb81094d14
prajwol <prajwol@ubuntu.ubuntu-domain>
2010-11-29 18:24:25 +0100

    - addcontact.java refactored

--- src/org/nebula/activities/AddContact.java
+++ src/org/nebula/activities/AddContact.java
@@ -1,5 +1,6 @@
 /*
- * author venkatesh, saad
+ * author: venkatesh, saad
+ * refactor: prajwol, venkatesh
  */
 
 package org.nebula.activities;
@@ -13,7 +14,6 @@ import org.nebula.models.Group;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,8 +21,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class AddContact extends Activity 
-{
+public class AddContact extends Activity implements
+		DialogInterface.OnClickListener,
+		DialogInterface.OnMultiChoiceClickListener {
 	private RESTGroupManager groupManager;
 
 	private Status status;
@@ -30,11 +31,10 @@ public class AddContact extends Activity
 	public static final int ADDCONTACT_FAILURE = 0;
 
 	private List<Group> groups;
-	private CharSequence[] groupNames;
+	private String[] groupNames;
 	private boolean[] selectedGroups;
 
-	public void onCreate(Bundle savedInstanceState) 
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_contact);
 
@@ -42,113 +42,96 @@ public class AddContact extends Activity
 	}
 
 	public void doSelectGroups(View v) {
-		showDialog(0);
-	}
-
-	protected Dialog onCreateDialog(int id) 
-	{
-		AlertDialog ad = null;
-
 		try {
 			groups = groupManager.retrieveGroups();
-			groupNames = new CharSequence[groups.size()]; 
-			selectedGroups = new boolean[groups.size()];
+		} catch (Exception e) {
+			Toast.makeText(v.getContext(),
+					"Groups could not be fetched",
+					Toast.LENGTH_LONG).show();
+			Log.e("nebula", "addcontact: " + e.getMessage());
+			return;
+		}
 
-			for(int i=0;i<groups.size();i++)
-			{
-				groupNames[i] = groups.get(i).getGroupName();
+		groupNames = new String[groups.size()];
+		selectedGroups = new boolean[groups.size()];
+
+		for (int i = 0; i < groups.size(); i++) {
+			groupNames[i] = groups.get(i).getGroupName();
+		}
+
+		new AlertDialog.Builder(this).setTitle("Select Groups")
+				.setMultiChoiceItems(groupNames, selectedGroups, this)
+				.setPositiveButton("OK", this).show();
+	}
+
+	public void onClick(DialogInterface dialog, int which) {
+		EditText text = (EditText) findViewById(R.id.etSelectedGroups);
+		String selectedGroupNames = "";
+		for (int i = 0; i < groupNames.length; i++) {
+			if (selectedGroups[i] == true) {
+				selectedGroupNames = selectedGroupNames + " [" + groupNames[i]
+						+ "] ";
 			}
+		}
+		text.setText(selectedGroupNames);
+	}
 
-			ad = new AlertDialog.Builder(this)
-			.setTitle("Select Groups")
-			.setMultiChoiceItems(
-					groupNames, 
-					selectedGroups,
-					new DialogSelectionClickHandler())
-					.setPositiveButton("OK",new OKHandler())
-					.create();
+	public void onClick(DialogInterface dialog, int clicked, boolean selected) {
+		// do nothing here :P
+	}
+
+	public void doAddContact(View v) {
+		String contactName = ((EditText) findViewById(R.id.etContactName))
+				.getText().toString();
+		String nebulaId = ((EditText) findViewById(R.id.etNebulaID)).getText()
+				.toString();
+
+		if (contactName == null || contactName.length() == 0
+				|| nebulaId == null || nebulaId.length() == 0) {
+			Toast.makeText(v.getContext(),
+					"ONE OR MORE THE FIELDS ARE NOT SPECIFIED",
+					Toast.LENGTH_LONG).show();
+			return;
+		}
+
+		try {
+			status = groupManager.addContact(contactName, nebulaId);
+
+			if (!status.isSuccess()) {
+				throw new Exception(status.getMessage());
+			}
 
 		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return ad;
-	}
-
-	protected class OKHandler implements DialogInterface.OnClickListener
-	{
-		String selectedGroupNames;
-		EditText text = (EditText) findViewById(R.id.etSelectedGroups);
-
-		public void onClick(DialogInterface dialog, int which) {
-			selectedGroupNames = "";
-			for (int i=0; i<groupNames.length;i++) {
-				if(selectedGroups[i]==true){
-					selectedGroupNames = selectedGroupNames + " [" + groupNames[i] + "] ";
-				}
-			}
-			text.setText(selectedGroupNames);
-		}
-	}
-
-	public void doAddContact(View v)
-	{
-		EditText etContactName = (EditText) findViewById(R.id.etContactName);
-		EditText etNebulaId = (EditText) findViewById(R.id.etNebulaID);
-
-		String contactName = etContactName.getText().toString();
-		String nebulaId = etNebulaId.getText().toString();
-
-		if(contactName == null || contactName.length() == 0
-				|| nebulaId == null || nebulaId.length() == 0)
-		{
-			Toast.makeText(v.getContext(), "ONE OR MORE THE FIELDS ARE NOT SPECIFIED",
+			Toast.makeText(this.getApplicationContext(), e.getMessage(),
 					Toast.LENGTH_LONG).show();
+			Log.e("nebula", "Add Contact: REST server error");
+
+			setResult(ADDCONTACT_FAILURE);
+			return;
 		}
-		else
-		{
+
+		for (int i = 0; i < selectedGroups.length; i++) {
+			// TODO Remove this try catch and club the job of adding the
+			// new contact
+			// into the mentioned groups also with the REST call above
 			try {
-				status  = groupManager.addContact(contactName, nebulaId);
-				for(int i=0;i<selectedGroups.length;i++)
-				{
-					//TODO Remove this try catch and club the job of adding the new contact 
-					//	into the mentioned groups also with the REST call above
-					try
-					{
-						if(selectedGroups[i])
-						{
-							groupManager.insertUserIntoGroup(groups.get(i), nebulaId);
-						}
-					}
-					catch(Exception e) {}
+				if (selectedGroups[i]) {
+					groupManager.insertUserIntoGroup(groups.get(i), nebulaId);
 				}
-			} 
-			catch (Exception e) 
-			{
-				Toast.makeText(this.getApplicationContext(), e.getMessage(),
-						Toast.LENGTH_LONG).show();
-				Log.v("nebula","Add Contact: REST server error");
-				e.printStackTrace();
-			}
-
-			Toast.makeText(this.getApplicationContext(), status.getMessage(),
-					Toast.LENGTH_LONG).show();
-
-			if(status !=null && status.isSuccess())
-			{
-				setResult(ADDCONTACT_SUCCESS);
-				finish();
+			} catch (Exception e) {
+				// continue to the next
+				Log.e("nebula", "addcontact: " + e.getMessage());
 			}
 		}
+
+		Toast.makeText(this.getApplicationContext(), status.getMessage(),
+				Toast.LENGTH_LONG).show();
+		setResult(ADDCONTACT_SUCCESS);
+		finish();
 	}
 
 	public void doBack(View v) {
 		setResult(ADDCONTACT_FAILURE);
 		finish();
-	}
-
-	protected class DialogSelectionClickHandler implements DialogInterface.OnMultiChoiceClickListener 
-	{
-		public void onClick(DialogInterface dialog, int clicked,
-				boolean selected) {}
 	}
 }

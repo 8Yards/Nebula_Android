94c78a093317db6c2ea3c70cdd2ad67475e19784
prajwol <prajwol@ubuntu.ubuntu-domain>
2010-12-13 15:48:03 +0100

    - changes to the files under rtp package to support PCM A-LAW and Demux

--- src/org/nebula/client/rtp/RTPReceiver.java
+++ src/org/nebula/client/rtp/RTPReceiver.java
@@ -4,44 +4,59 @@
  */
 package org.nebula.client.rtp;
 
+import java.util.Enumeration;
+
 import jlibrtp.DataFrame;
 import jlibrtp.Participant;
 import jlibrtp.RTPAppIntf;
 import jlibrtp.RTPSession;
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioRecord;
-import android.media.AudioTrack;
+
+import org.sipdroid.codecs.G711;
+
 import android.util.Log;
 
 public class RTPReceiver implements RTPAppIntf {
 	private RTPSession rtpReceiver = null;
-	private ReceiverConfiguration conf = new ReceiverConfiguration();
 
-	public void stopPlaying() {
-		// TODO: session is shared between Reciever and Sender - handle the
-		// integrity
-		rtpReceiver.endSession();
-		conf.audioTrack.stop();
+	public RTPReceiver(RTPSession rtpSession) {
+		this.rtpReceiver = rtpSession;
 	}
 
-	/*
-	 * Start playing RTP
-	 */
-	// TODO:: check if play is done in separate thread itself
+	public void stopPlaying() {
+		Enumeration<Participant> enuPart = rtpReceiver.getParticipants();
+		if (enuPart != null) {
+			while (enuPart.hasMoreElements()) {
+				enuPart.nextElement().track.stop();
+			}
+		}
+		rtpReceiver.endSession();
+		Log.v("nebula", "rtpReceiver: stopped");
+	}
+
 	public void startPlaying() {
 		new Thread(new Runnable() {
 			public void run() {
-				conf.audioTrack.play();
-				Log.v("nebula", "servicereceiver: inPlayer...");
+				Enumeration<Participant> enuPart = rtpReceiver
+						.getParticipants();
+				if (enuPart != null) {
+					while (enuPart.hasMoreElements()) {
+						enuPart.nextElement().track.play();
+					}
+				}
+				Log.v("nebula", "rtpReceiver: inPlayer...");
 			}
 		}).start();
 	}
 
 	public void receiveData(final DataFrame frame, final Participant participant) {
 		final byte[] data = frame.getConcatenatedData();
-		conf.audioTrack.write(data, 0, data.length);		
-		Log.v("nebula", "servicereceiver: received - " + data.length);
+		short[] linData = new short[RTPReceiverConfiguration.instance.frameSize];
+
+		G711.alaw2linear(data, linData, linData.length);
+		participant.track.write(linData, 0, linData.length);
+		Log.v("jlibrtp2", participant.getSSRC() + " playing");
+
+		Log.v("nebula", "rtpReceiver: received - " + data.length);
 	}
 
 	// interface method
@@ -51,19 +66,5 @@ public class RTPReceiver implements RTPAppIntf {
 
 	// interface method
 	public void userEvent(int type, Participant[] participant) {
-	}
-
-	private class ReceiverConfiguration {
-		public int frequency = 11025;
-		public int channelConfiguration = AudioFormat.CHANNEL_CONFIGURATION_MONO;
-		public int audioEncoding = AudioFormat.ENCODING_PCM_16BIT;
-
-		public int bufferSize = AudioRecord.getMinBufferSize(frequency,
-				channelConfiguration, audioEncoding);
-		public AudioTrack audioTrack = new AudioTrack(
-				AudioManager.STREAM_MUSIC, 11025,
-				AudioFormat.CHANNEL_CONFIGURATION_MONO,
-				AudioFormat.ENCODING_PCM_16BIT, bufferSize,
-				AudioTrack.MODE_STREAM);
 	}
 }
